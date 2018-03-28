0449b041fd71baf31d965ab31aaf7ccab9aa8932
HA <haydnba@gmail.com>
2018-03-28 13:09:31 +0100

    add display headlines on load function Relates #5

--- public/dom.js
+++ public/dom.js
@@ -1,6 +1,7 @@
 (function () {
   // Document Selectors here:
-
+  const newsTitle = document.querySelector('#js-newsTitle');
+  const newsList = document.querySelector('#js-newsList');
 
   // Variables here:
 
@@ -12,14 +13,26 @@
 
   // DOM manipulation on response:
 
-  function displayHeadlines(x) {
-    console.log('x');
+  function displayHeadlines(newsObject) {
+    // console.log(newsObject);
+    var keys =['source.name', 'title', 'url', 'publishedAt'];
+    newsObject.articles.forEach(function(item) {
+      var article = {
+        source: item.source.name,
+        title: item.title,
+        url: item.url,
+        date: (item.publishedAt).slice(0, 10),
+        time: (item.publishedAt).slice(11, 19)
+      };
+      console.log(article);
+    })
+    
   }
 
 
   // Function Declaration:
  
-  //   Fetch requests
+  // Fetch request
   function fetch(method, url, callback) {
     var xhr = new XMLHttpRequest();
 
