924e13d485c89d050d0d27d838529e115d503d57
HA <haydnba@gmail.com>
2018-03-29 13:09:20 +0100

    minor fix in dom

--- public/dom.js
+++ public/dom.js
@@ -33,7 +33,6 @@
 			let span = document.createElement('span');
 			span.classList.add('flag-icon', 'flag-icon-' + countryObject[country]);
 			button.classList.add('country__button');
-			button.textContent = country + ' ';
 			button.appendChild(span);
 			button.value = countryObject[country];
 			countriesContainer.appendChild(button);
